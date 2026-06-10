15c56ebd1b12f8c5f02e2814ad2eb0657bec44d0
tutran27 <dinhtu7cfc@gmail.com>
2026-06-11 00:06:04 +0700

    Update: openai/whisper-large-v3-turbo asr

--- asr.py
+++ asr.py
@@ -1,32 +1,42 @@
+import os
+
 import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
-from datasets import load_dataset
 
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
-torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
 model_id = "openai/whisper-large-v3-turbo"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    model_id, dtype=dtype, low_cpu_mem_usage=True, use_safetensors=True
 )
 model.to(device)
 
 processor = AutoProcessor.from_pretrained(model_id)
+processor.tokenizer.clean_up_tokenization_spaces = False
+model.generation_config.forced_decoder_ids = None
+model.generation_config.suppress_tokens = None
+model.generation_config.begin_suppress_tokens = None
 
 pipe = pipeline(
     "automatic-speech-recognition",
     model=model,
     tokenizer=processor.tokenizer,
     feature_extractor=processor.feature_extractor,
-    torch_dtype=torch_dtype,
+    dtype=dtype,
     device=device,
-    device_map="auto",  # Explicitly enable model parallelism
 )
 
-dataset = load_dataset("distil-whisper/librispeech_long", "clean", split="validation")
-sample = dataset[0]["audio"]
+audio_path = "outputs/standard_Gia Bảo.wav"
 
-result = pipe(sample)
+if not os.path.exists(audio_path):
+    raise FileNotFoundError(f"Khong tim thay file audio: {audio_path}")
+
+result = pipe(
+    audio_path,
+    return_timestamps=True,
+    generate_kwargs={"task": "transcribe", "language": "vi"},
+)
 print(result["text"])
